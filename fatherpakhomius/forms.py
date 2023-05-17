9349e08c6e848bb0aae1d8fbe2f80421d3c0a7a6
Murgany <76653393+Murgany@users.noreply.github.com>
2023-05-17 21:43:13 +0300

    Update forms.py

--- fatherpakhomius/forms.py
+++ fatherpakhomius/forms.py
@@ -2,11 +2,7 @@ from django import forms
 from .models import UserMessage
 
 
-class ContactForm(forms.Form):
-    subject = forms.CharField(max_length=100)
-    email_address = forms.EmailField(max_length=150)
-    message = forms.CharField(widget=forms.Textarea, max_length=2000)
-
+# Usr message form | Contact us page
 
 class UserMessageForm(forms.ModelForm):
     class Meta:
