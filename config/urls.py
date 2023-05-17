8e75dcf0c58e9da3d6ea17683ef6bbd34b15bbb7
Murgany <76653393+Murgany@users.noreply.github.com>
2023-05-17 22:07:27 +0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,23 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib import admin
-from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
+
+
+# Set up project urls
 
 urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('', include('fatherpakhomius.urls')),
-    # path('', include('admin_black.urls')),
-    # path('__debug__/', include('debug_toolbar.urls')),
+
+    # I had to include this here because I'm using the {% set_language %} tag to switch between languages
     path('i18n/', include('django.conf.urls.i18n')),
+
     # If no prefix is given, use the default language
     prefix_default_language=False
 )
 
-
-#urlpatterns = (
-#     path('admin/', admin.site.urls),
-#     path('', include('fatherpakhomius.urls')),
-#     # path('__debug__/', include('debug_toolbar.urls')),
-#     path('i18n/', include('django.conf.urls.i18n')),
-# )
