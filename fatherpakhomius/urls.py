0605f17bd77c3e604f25890d886a4e94d9e20cca
Murgany <76653393+Murgany@users.noreply.github.com>
2023-05-17 21:42:14 +0300

    Update urls.py

--- fatherpakhomius/urls.py
+++ fatherpakhomius/urls.py
@@ -1,34 +1,28 @@
 from django.urls import path
-# successView #, audio_download,ContactView
-from .views import category_list, sermon_list_by_category, book_list_by_category, PostView, IndexView, BookListView, SermonsView, LatestBookListView, OtherSermonsView, OtherBooksView, contactView, PostDetailView
+from .views import *
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+# Set up the app urls
+
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
-    path('articles', PostView.as_view(), name='articles'),
-    path('articles/<int:pk>', PostDetailView.as_view(), name='articles'),
+                  path('', IndexView.as_view(), name='index'),
+                  path('articles', PostView.as_view(), name='articles'),
+                  path('articles/<int:pk>', PostDetailView.as_view(), name='articles'),
 
-    path('sermons', SermonsView.as_view(), name='sermons'),
-    path('other_sermons', OtherSermonsView.as_view(), name='other_sermons'),
-    #   path('latest_sermons', LatestSermons.as_view(), name='latest_sermons'),
+                  path('sermons', SermonsView.as_view(), name='sermons'),
+                  path('other_sermons', OtherSermonsView.as_view(), name='other_sermons'),
 
-    path('book_list', BookListView.as_view(), name='book_list'),
-    path('latest_books', LatestBookListView.as_view(), name='latest_books'),
-    path('other_books', OtherBooksView.as_view(), name='other_books'),
-    # path('book_categories', BookCategoryView.as_view(), name='book_categories'),
-    # path('book_detail/<int:pk>', BookDetailView.as_view(), name='book_detail'),
+                  path('book_list', BookListView.as_view(), name='book_list'),
+                  path('latest_books', LatestBookListView.as_view(), name='latest_books'),
+                  path('other_books', OtherBooksView.as_view(), name='other_books'),
 
-    # path('categories/', category_list, name='categories'),
-    # path('sermon_categories/<int:pk>/', SermonCategoryDetailView.as_view(), name='sermon_category_detail'),
-    # path('book_categories/<int:pk>/', BookCategoryDetailView.as_view(), name='book_category_detail'),
+                  path('categories/', category_list, name='category_list'),
+                  path('book-categories/<int:category_id>/books/', book_list_by_category, name='book_category_detail'),
+                  path('sermon-categories/<int:category_id>/sermons/', sermon_list_by_category, name='sermon_category_detail'),
 
-    path('categories/', category_list, name='category_list'),
-    path('book-categories/<int:category_id>/books/', book_list_by_category, name='book_category_detail'),
-    path('sermon-categories/<int:category_id>/sermons/', sermon_list_by_category, name='sermon_category_detail'),
+                  path('contact/', contact_view, name='contact'),
+                  path('search_results', views.search_results, name='search-results'),
 
-    path('contact/', contactView, name='contact'),
-    path('search_results', views.search_results, name='search-results'),
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # Static files configuration
