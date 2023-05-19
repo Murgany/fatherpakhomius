7ab7db1a13e5b9ffc99893d0f670e7c46a3b92ca
Murgany <76653393+Murgany@users.noreply.github.com>
2023-05-19 20:57:57 +0300

    delete test.py

--- fatherpakhomius/templates/test.py
+++
@@ -1,8 +0,0 @@
-def list_sorter(objects_list):
-
-    return sorted(objects_list)
-
-
-print(list_sorter([9, 6, 3 ,1]))   #[1, 3, 6, 9]
-
-print(list_sorter(["d", "c", "b", "a" ]))   #['a', 'b', 'c', 'd']
